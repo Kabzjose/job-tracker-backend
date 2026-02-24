51865df8517a3f04eba56c4d6c7b9e537cbe60c1
Joseph Ndungu <josekabz00@gmail.com>
2026-02-24 14:00:19 +0300

    initial commit

---
+++ src/db.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+
+const {Pool} = require("pg")
+
+
+module.exports = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+
+
+    max:20,
+    idleTimeoutMillis:30000,
+    ConnectionTimeoutMillis:2000,
+})
+
+ 
---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+
+const express=require("express")
+const cors=require("cors")
+const authRoutes=require("./routes/auth")
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+app.use("/auth",authRoutes)
+
+app.get("/", (req,res)=>{
+    res.send("Job Tracker API running")
+})
+app.listen(5000,()=>{
+    console.log("server running on port 5000")
+})
