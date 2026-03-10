c575642c403d50bc6a2d5c3ba993fa16cf8637b9
Joseph Ndungu <josekabz00@gmail.com>
2026-03-10 20:12:24 +0300

    testing connection

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const { Pool } = require("pg")
 
+
+console.log("DB URL:", process.env.DATABASE_URL)
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
