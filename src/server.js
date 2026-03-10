f0bf2ec436346a798e380eeb152907ad0865a82f
Joseph Ndungu <josekabz00@gmail.com>
2026-03-10 22:18:57 +0300

    removed .env

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const errorHandler = require("./middleware/errorHandler")
 
 const app=express()
 
-//| http://localhost:5173
+//https://job-tracker-wheat-kappa.vercel.app http://localhost:5173
 app.use(cors({
-  origin: "https://job-tracker-wheat-kappa.vercel.app ",
+  origin: "http://localhost:5173 ",
   credentials: true
 }))
 app.use(express.json())
